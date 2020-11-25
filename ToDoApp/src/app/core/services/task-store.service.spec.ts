a795542e159e66faa458e20a0d0073f5fb952b68
sandhyakj <74828181+sandhyakj@users.noreply.github.com>
2020-11-25 00:12:09 +0000

    Update task-store.service.spec.ts

--- ToDoApp/src/app/core/services/task-store.service.spec.ts
+++ ToDoApp/src/app/core/services/task-store.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TaskStoreService } from './task-store.service';
+import { HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('TaskStoreService', () => {
   let service: TaskStoreService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(TaskStoreService);
   });
 
@@ -14,3 +19,4 @@ describe('TaskStoreService', () => {
     expect(service).toBeTruthy();
   });
 });
+
